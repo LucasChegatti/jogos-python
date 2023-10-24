9de4d21b3938c1bcbe04761345fb5f5f17aeaae8
LucasChegatti <lucas.chegatti@hotmail.com>
2023-10-23 22:18:17 -0300

    Alterando while para for

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,20 +3,29 @@ print("Bem vindo ao nosso jogo de adivinhação!")
 print("***************************************")
 
 numero_secreto = 3
+total_de_tentativas = 3
 
-chute_input = input("Digite seu numero da sorte: ")
+for rodada in range(1, total_de_tentativas + 1):
+    print("Rodada {} de {}".format(rodada, total_de_tentativas))
+    chute_input = input("Digite seu numero da sorte entre 1 e 100: ")
+    print("Você digitou o numero: ", chute_input)
+    chute = int(chute_input)
 
-print("Você digitou o numero: ", chute_input)
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um numero entre 1 e 100")
+        continue
 
-chute = int(chute_input)
+    chute_certo = chute == numero_secreto
+    chute_menor = chute < numero_secreto
+    chute_maior = chute > numero_secreto
 
-if(numero_secreto == chute):
-    print("Você acertou!!!")
-else:
-    print("Você errou!!!")
+    if(chute_certo):
+        print("Você acertou!!!")
+        break
+    else:
+        if(chute_menor):
+            print("Você errou! O seu chute foi menor que o numero secreto!")
+        elif(chute_maior):
+            print("Você errou! O seu chute foi maior que o numero secreto!")
 
 print("Fim do jogo!")
-
-idade1 = "10"
-idade2 = "20"
-print(idade1 + idade2)
